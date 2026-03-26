6372cd51266d76ab3e9c8065700d134be976512a
Jose David Colmenarez Gil <jcolmenarezgil@byupathway.edu>
2026-03-25 20:21:37 -0400

    feat: setup google/ms auth packages and user-secrets

--- Program.cs
+++ Program.cs
@@ -1,10 +1,34 @@
 using PetStore.Components;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.MicrosoftAccount;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add services de Autenticate and Cookies
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddGoogle(googleOptions =>
+    {
+        googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"]!;
+        googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"]!;
+    })
+    .AddMicrosoftAccount(msOptions =>
+    {
+        msOptions.ClientId = builder.Configuration["Authentication:Microsoft:ClientId"]!;
+        msOptions.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"]!;
+    });
+
+// Enable the state of authentication in Blazor
+builder.Services.AddCascadingAuthenticationState();
 
 var app = builder.Build();
 
